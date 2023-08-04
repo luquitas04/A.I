85eb632a813f9d90c918db56a853a06eee5db955
Lucas <lucbenlopez7@gmail.com>
2023-08-04 10:39:00 -0300

    feat

--- handTracking/hand_tracking.py
+++ handTracking/hand_tracking.py
@@ -9,15 +9,18 @@ cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     static_image_mode = False,
     max_num_hands = 1,
-    min_detection_confidence = 0.5,
-    min_tracking_confidence = 0.5,
+    min_detection_confidence = 0.3,
+    min_tracking_confidence = 0.3,
 ) as hands: 
     while cap.isOpened():
         success, image = cap.read() 
 
         if not success: 
             print("No se puede obtener la imagen de la cámara. Saliendo...")
-            
+        
+        # Reducir la resolucion de Imagen
+        image = cv2.resize(image, (640,480))
+
         # Convertir la imagen de BGR a RGB 
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -36,53 +39,86 @@ with mp_hands.Hands(
                     # Dibujar circulos en los puntos de referencia 
                     cv2.circle(image, pixel_coords, 5, (0,255,0), -1)
                 
-                hand_landmarks_list = [
-                    mp_hands.HandLandmark.WRIST,
-                    mp_hands.HandLandmark.THUMB_CMC,
-                    mp_hands.HandLandmark.THUMB_MCP,
-                    mp_hands.HandLandmark.THUMB_IP,
-                    mp_hands.HandLandmark.THUMB_TIP,
-                    mp_hands.HandLandmark.INDEX_FINGER_MCP,
-                    mp_hands.HandLandmark.INDEX_FINGER_PIP,
-                    mp_hands.HandLandmark.INDEX_FINGER_TIP,
-                    mp_hands.HandLandmark.MIDDLE_FINGER_MCP,
-                    mp_hands.HandLandmark.MIDDLE_FINGER_PIP,
-                    mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
-                    mp_hands.HandLandmark.RING_FINGER_MCP,
-                    mp_hands.HandLandmark.RING_FINGER_PIP,
-                    mp_hands.HandLandmark.RING_FINGER_TIP,
-                    mp_hands.HandLandmark.PINKY_MCP,
-                    mp_hands.HandLandmark.PINKY_PIP,
-                    mp_hands.HandLandmark.PINKY_TIP,
-                ]                                   
-                for i in range(0, len(hand_landmarks_list) -1 , 4):
-                    for j in range(i,i + 4):
-                        if (
-                            not results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]]
-                            or not results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]]
+                # hand_landmarks_list = [
+                #     mp_hands.HandLandmark.WRIST,
+                #     mp_hands.HandLandmark.THUMB_CMC,
+                #     mp_hands.HandLandmark.THUMB_MCP,
+                #     mp_hands.HandLandmark.THUMB_IP,
+                #     mp_hands.HandLandmark.THUMB_TIP,
+                #     mp_hands.HandLandmark.INDEX_FINGER_MCP,
+                #     mp_hands.HandLandmark.INDEX_FINGER_PIP,
+                #     mp_hands.HandLandmark.INDEX_FINGER_TIP,
+                #     mp_hands.HandLandmark.MIDDLE_FINGER_MCP,
+                #     mp_hands.HandLandmark.MIDDLE_FINGER_PIP,
+                #     mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
+                #     mp_hands.HandLandmark.RING_FINGER_MCP,
+                #     mp_hands.HandLandmark.RING_FINGER_PIP,  
+                #     mp_hands.HandLandmark.RING_FINGER_TIP,
+                #     mp_hands.HandLandmark.PINKY_MCP,
+                #     mp_hands.HandLandmark.PINKY_PIP,
+                #     mp_hands.HandLandmark.PINKY_TIP,
+                # ]                                   
+                # for i in range(0, len(hand_landmarks_list) -1 , 4):
+                #     for j in range(i,i + 4):
+                #         if (
+                #             not results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]]
+                #             or not results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]]
+                #         ):
+                #             break
+                #         start_point = (
+                #                 int(
+                #                     results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]].x
+                #                     * image.shape[1]
+                #                 ),
+                #                 int(
+                #                     results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]].y
+                #                     * image.shape[0]
+                #                 ),
+                #             )
+                #         end_point = (
+                #                 int(
+                #                     results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]].x
+                #                     * image.shape[1]
+                #                 ),
+                #                 int(
+                #                     results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]].y
+                #                     * image.shape[0]
+                #                 ),
+                #             )
+                #         cv2.line( image, start_point, end_point, (0,255,0),2 )
+
+                # Dibujamos las lineas
+                connections = mp_hands.HAND_CONNECTIONS
+                for connection in connections:
+                    start_point_idx = connection[0]
+                    end_point_idx = connection[1]
+
+                    if (
+                            not results.multi_hand_landmarks[0].landmark[start_point_idx]
+                            or not results.multi_hand_landmarks[0].landmark[end_point_idx]
                         ):
-                            break
-                        start_point = (
+                            continue
+                    start_coords = (
                                 int(
-                                    results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]].x
+                                    results.multi_hand_landmarks[0].landmark[start_point_idx].x
                                     * image.shape[1]
                                 ),
                                 int(
-                                    results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j]].y
+                                    results.multi_hand_landmarks[0].landmark[start_point_idx].y
                                     * image.shape[0]
                                 ),
                             )
-                        end_point = (
+                    end_coords = (
                                 int(
-                                    results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]].x
+                                    results.multi_hand_landmarks[0].landmark[end_point_idx].x
                                     * image.shape[1]
                                 ),
                                 int(
-                                    results.multi_hand_landmarks[0].landmark[hand_landmarks_list[j + 1]].y
+                                    results.multi_hand_landmarks[0].landmark[end_point_idx].y
                                     * image.shape[0]
                                 ),
                             )
-                        cv2.line( image, start_point, end_point, (0,255,0),2 )
+                    cv2.line( image, start_coords, end_coords, (0,255,0),2 )
                 # Mostrar la imagen con el seguimiento de la mano
                 cv2.imshow("Hand Tracking", image)
 
